c98e8541f0e1022241948aefdcad2347a590f2ae
ChiFeng <chifengchou@truetel.com>
2014-09-26 19:11:35 +0800

    [TEMP] 4sum

--- src/4s.py
+++ src/4s.py
@@ -6,9 +6,10 @@ __author__ = 'cfchou'
 class Solution:
     # @return a list of lists of length 4, [[val1,val2,val3,val4]]
     def fourSum(self, num, target):
-        if None == num or 0 == len(num):
+        num2 = filter(lambda n: n <= target, num)
+        if None == num2 or 0 == len(num2):
             return []
-        sortedNum = sorted(num)
+        sortedNum = sorted(num2)
         shift = sortedNum[0]
         normalizedNum = map(lambda x: x - shift, sortedNum)
         normalizedTarget = target - 4 * shift
@@ -46,6 +47,8 @@ class Solution:
         # { v1: [n1, n4, n5], v2: [ n2, n3], ... }
         tdict = {}
         for i in range(0, len(num)):
+            if num[i] > target:
+                continue
             v = target - num[i]
             jj = tdict.get(v, [])
             ret += map(lambda j: (j, i), jj)
@@ -54,17 +57,6 @@ class Solution:
                 tdict[num[i]].append(i)
             else:
                 tdict[num[i]] = [i]
-            '''
-            if j:
-                # FIX:
-                # All pairs should be considered. e.g.
-                # [2, 2, 2], target = 4
-                # there should be 3 pairs.
-                del tdict[v]
-                ret += (j, i)
-            else:
-                tdict[num[i]] = i
-            '''
         return ret
 
     def testTwoSum(self, num, target):
@@ -80,10 +72,10 @@ class Solution:
 num = [1, 0, -1, 0, -2, 2]
 target = 0
 sol = Solution()
-print(sol.testTwoSum(num, target))
-print(sol.fourSum(num, target))
+#print(sol.testTwoSum(num, target))
+#print(sol.fourSum(num, target))
 
 num = [-500,-481,-480,-469,-437,-423,-408,-403,-397,-381,-379,-377,-353,-347,-337,-327,-313,-307,-299,-278,-265,-258,-235,-227,-225,-193,-192,-177,-176,-173,-170,-164,-162,-157,-147,-118,-115,-83,-64,-46,-36,-35,-11,0,0,33,40,51,54,74,93,101,104,105,112,112,116,129,133,146,152,157,158,166,177,183,186,220,263,273,320,328,332,356,357,363,372,397,399,420,422,429,433,451,464,484,485,498,499]
 target = 2139
-print(sol.fourSum(num, target))
+#print(sol.fourSum(num, target))
 
