bbc7318002411b64ee4e7830f73bab3f14eaddd8
cfchou <cfchou@gmail.com>
2014-10-01 23:26:14 +0800

    Maximum Product Subarray

--- src/Mps.py
+++ src/Mps.py
@@ -1,4 +1,5 @@
 __author__ = 'cfchou'
+import sys
 
 # https://oj.leetcode.com/problems/maximum-product-subarray/
 
@@ -6,6 +7,18 @@ class Solution:
     # @param A, a list of integers
     # @return an integer
     def maxProduct(self, A):
+        cMax = A[0]
+        pMin = A[0]
+        pMax = A[0]
+        for e in A[1:]:
+            a = e * pMin
+            b = e * pMax
+            pMin = min([e, a, b])
+            pMax = max([e, a, b])
+            cMax = max(cMax, pMax)
+        return cMax
+
+    def maxProductTry(self, A):
         mx = []
         for i in range(len(A)):
             # for j in range(len(A) - i):
@@ -16,10 +29,9 @@ class Solution:
                     if j + i <= len(A) - 1:
                         v = mx[j][0] * mx[j + 1][i - 1]
                         mx[j].append(v)
-
         return max(map(lambda a: max(a), mx))
 
 sol = Solution()
-arr = [2, 3, -2, 4]
+arr = [2, -3, -1, -2, 4]
 print(sol.maxProduct(arr))
 
