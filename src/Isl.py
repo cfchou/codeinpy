2605317f1fab0523fc4058c4f56da88ba21a3026
ChiFeng <chifengchou@truetel.com>
2014-10-02 13:45:38 +0800

    [Temp] Insertion Sort List

---
+++ src/Isl.py
@@ -0,0 +1,141 @@
+__author__ = 'chifeng'
+
+# https://oj.leetcode.com/problems/insertion-sort-list/
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a ListNode
+    def insertionSortList(self, head):
+        hl = self.listNodeToList(head)
+        shuffed = self.suffle(hl)
+        sl = self.insertionSort(shuffed)
+        return self.listToListNode(sl)
+
+    def listNodeToList(self, head):
+        ret = []
+        while head:
+            ret.append(head.v)
+            head = head.next
+        return ret
+
+    def listToListNode(self, lst):
+        tmp = map(ListNode, lst)
+        ret = tmp[len(tmp) - 1]
+        for i in range(len(tmp) - 2, -1, -1):
+            tmp[i].next = ret
+        return ret
+
+    def shuffle(self, lst):
+        pass
+
+    def insertionSort(self, lst):
+        pass
+
+
+    def insertionSortListTry2(self, head):
+        def insertSorted(sortedList, node):
+            if None == sortedList:
+                node.next = None
+                return node
+            if node.val >= sortedList.val:
+                node.next = sortedList
+                return node
+            else:
+                sortedList.next = insertSorted(sortedList.next, node)
+                return sortedList
+
+        def reverseList(head):
+            if None == head:
+                return None
+            reversedHead = head
+            head = head.next
+            reversedHead.next = None
+            while head:
+                nnext = head.next
+                head.next = reversedHead
+                reversedHead = head
+                head = nnext
+            return reversedHead
+
+        sortedHead = None
+        while head:
+            nnext = head.next
+            sortedHead = insertSorted(sortedHead, head)
+            head = nnext
+        return reverseList(sortedHead)
+
+        # Receive "maximum recursion depth exceeded" error as recursion is not "pythonic"
+        # and tail recursion is not supported.
+        def reverseListRecur(head):
+            if None == head:
+                return None, None
+            if None == head.next:
+                return head, head
+            tmp = head
+            head, end = reverseListRecur(head.next)
+            end.next = tmp
+            tmp.next = None
+            return head, tmp
+
+    def insertionSortListTry(self, head):
+        def insertSorted(sortedList, node):
+            if None == sortedList:
+                node.next = None
+                return node
+            if node.val <= sortedList.val:
+                node.next = sortedList
+                return node
+            else:
+                sortedList.next = insertSorted(sortedList.next, node)
+                return sortedList
+
+        sortedHead = None
+        nnext = None
+        while head:
+            nnext = head.next
+            sortedHead = insertSorted(sortedHead, head)
+            head = nnext
+        return sortedHead
+
+def printNodes(head):
+    while head:
+        print(head.val)
+        head = head.next
+
+def createList(n):
+    if n < 1:
+        return None
+    start = ListNode(0)
+    prev = start
+    for i in range(1, n):
+        prev.next = ListNode(i)
+        prev = prev.next
+    return start
+
+sol = Solution()
+
+lst = createList(5000)
+printNodes(lst)
+ret = sol.insertionSortList(lst)
+printNodes(ret)
+
+
+# arr = map(lambda i: ListNode(i), range(10))
+# # 0,9,2,8,3
+# arr[0].next = arr[9]
+# arr[9].next = arr[2]
+# arr[2].next = arr[8]
+# arr[8].next = arr[3]
+# printNodes(arr[0])
+# ret = sol.insertionSortList(arr[0])
+# printNodes(ret)
+
+
+
+
