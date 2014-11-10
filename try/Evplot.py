d824c3f9d23ed6d2c5ce476e813723c657239cb9
ChiFeng <chifengchou@truetel.com>
2014-11-10 14:56:27 +0800

    [Try] matplotlib

---
+++ try/Evplot.py
@@ -0,0 +1,56 @@
+__author__ = 'chifeng'
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import *
+from dateutil.parser import *
+import matplotlib.dates as mdates
+
+import sys
+
+category = {
+    0: '\"UNKNOWN\"',
+    1: '\"LOG_FILE\"',
+    2: '\"SYSTEM\"',
+    3: '\"DEVICE\"',
+    4: '\"MEDIA\"',
+    5: '\"ACCESSIBILITY\"',
+    6: '\"ICS_CTRL\"',
+    7: '\"VEHICLE_STATE\"',
+    8: '\"ONSTAR\"',
+    9: '\"HVAC\"'
+}
+
+def datetimeToMillies(d):
+    epoch = datetime.utcfromtimestamp(0)
+    # timedelta.total_seconds() return a float
+    return (d - epoch).total_seconds() * 1000.0
+
+fig, ax = plt.subplots(1, 1, figsize=(15, 5))
+
+path = "./postEvent.csv.out.csv"
+csv = pd.read_csv(path)
+
+
+# fromtimestamp() accepts timestamp in seconds and add tz to it.
+# utcfromtimestamp() doesn't add tz.
+utc = csv.eventUTCTime.apply(
+    lambda x: datetime.utcfromtimestamp(x / 1000))  # Series
+csv['DatetimeUTC'] = utc
+
+cat = csv.loc[:, ['DatetimeUTC', 'category']]  # DataFrame
+cat.plot(kind='scatter', x='DatetimeUTC', y='category', ax=ax)
+ax.yaxis.grid()
+ax.yaxis.set_ticklabels(category.values())
+
+
+# date2num() returns a float since 0001-01-01 00:00:00 UTC *plus* *one*.
+#t = csv.eventUTCTime.apply(
+#    lambda x: mdates.date2num(datetime.utcfromtimestamp(x / 1000)))  # Series
+# dat = pd.DataFrame({'category': cat.category, 'utc': t})
+# plt.plot_date(dat.utc, dat.category, fmt="bo", tz=None, xdate=True)
+
+
+
+
